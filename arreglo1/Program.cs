f0bfa4db7c768e146f1aeea9d5ab180391a51121
USUARIO <ervinroch@uamv.edu.ni>
2026-05-06 12:24:22 -0600

    fix:agregue validaciones

--- arreglo1/Program.cs
+++ arreglo1/Program.cs
@@ -1,6 +1,4 @@
-﻿    ﻿using System;
-
-// Leer 5 edades y mostrar la edad promedio 
+﻿// Leer 5 edades y mostrar la edad promedio 
 
 int[] edades = new int[5];
 int suma = 0;
@@ -8,8 +6,16 @@ double promedio = 0;
 
 for(int i=0; i < edades.Length; i++)
 {
+try
+{
     Console.WriteLine("Dime la edad " + (i+1) + ":" );
     edades[i] = int.Parse(Console.ReadLine()!);
+}
+catch
+{
+    Console.WriteLine("Edad inválida. Por favor, ingresa un número entero.");
+    i--; // Volver a pedir la misma posición
+}
 }
 
 foreach(int edad in edades)
